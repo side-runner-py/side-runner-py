f1727d53e92ccddaa854dc14c65c04778364191c
yaamai <ya_amai@yahoo.co.jp>
2019-04-03 05:01:23 +0000

    Remove unnecessary main() unit test

--- tests/unit/test_main.py
+++ tests/unit/test_main.py
@@ -8,8 +8,10 @@ def test_get_side_file_list_by_glob():
         assert list(main._get_side_file_list_by_glob('')) == []
 
 
-def test_get_side_fixed_file_list_by_glob():
-    assert list(main._get_side_file_list_by_glob('/tmp/a.json')) == ['/tmp/a.json']
+def test_get_side_fixed_file_list_by_glob(tmp_path):
+    sidefile = tmp_path / "a.json"
+    sidefile.write_text("[]")
+    assert len(list(main._get_side_file_list_by_glob(str(sidefile)))) == 1
 
 
 def test_main_with_glob_no_match(mocker):
@@ -18,33 +20,3 @@ def test_main_with_glob_no_match(mocker):
     execute_side_file_mock = mocker.patch('side_runner_py.main._execute_side_file')
     main.main()
     assert execute_side_file_mock.call_count == 0
-
-
-def test_main_with_glob_one_match(mocker):
-    mocker.patch('side_runner_py.main.with_retry')
-    mocker.patch('side_runner_py.main.SIDEProjectManager')
-
-    path_file_mock = mocker.Mock()
-    path_file_mock.stem = "foobar-1"
-    path_mock = mocker.Mock()
-    path_mock.parent.glob.return_value = [path_file_mock]
-    mocker.patch('side_runner_py.main.pathlib.Path').return_value = path_mock
-
-    execute_side_file_mock = mocker.patch('side_runner_py.main._execute_side_file')
-    main.main()
-    assert execute_side_file_mock.call_count == 1
-
-
-def test_main_with_glob_multi_match(mocker):
-    mocker.patch('side_runner_py.main.with_retry')
-    mocker.patch('side_runner_py.main.SIDEProjectManager')
-
-    path_file_mock = mocker.Mock()
-    path_file_mock.stem = "foobar-1"
-    path_mock = mocker.Mock()
-    path_mock.parent.glob.return_value = [path_file_mock, path_file_mock]
-    mocker.patch('side_runner_py.main.pathlib.Path').return_value = path_mock
-
-    execute_side_file_mock = mocker.patch('side_runner_py.main._execute_side_file')
-    main.main()
-    assert execute_side_file_mock.call_count == 2

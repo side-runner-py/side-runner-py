a9c7edd57f16ac0ccee8e57e310504795f8b05f7
yaamai <ya_amai@yahoo.co.jp>
2019-04-03 00:49:38 +0000

    Add unittest for parameterized tests

--- tests/unit/test_side.py
+++ tests/unit/test_side.py
@@ -48,4 +48,35 @@ class TestSIDEProjectManager:
         assert tests['foobar']['params'] == params[0]['params']
 
     def test_expand_test_with_params(self, mocker):
-        pass
+        params = [
+            {
+                "test_name": "Input form",
+                "params_type": "list",
+                "params": [
+                    {"message": "Foo"},
+                    {"message": "Bar"}
+                ]
+            },
+        ]
+        mocker.patch('side_runner_py.side.open')
+        mocker.patch('json.load').return_value = params
+
+        tests = {'foobar': {"id": "foobar", "name": "Input form"}}
+        side_manager = SIDEProjectManager()
+        side_manager._attach_params('foobar.json', tests)
+        test_suites = [{'id': 'foobar', 'tests': ['foobar'], 'name': 'foobar'}]
+        test_suites, tests = side_manager._expand_test_with_params(test_suites, tests)
+        assert test_suites == [
+            {'id': 'foobar-0', 'name': 'foobar-0', 'tests': ['foobar-0']},
+            {'id': 'foobar-1', 'name': 'foobar-1', 'tests': ['foobar-1']}]
+        assert tests == {
+            'foobar-0': {
+                'id': 'foobar-0',
+                'name': 'Input form-0',
+                'params': [{'message': 'Foo'}, {'message': 'Bar'}]
+            },
+            'foobar-1': {
+                'id': 'foobar-1',
+                'name': 'Input form-1',
+                'params': [{'message': 'Foo'}, {'message': 'Bar'}]
+            }}

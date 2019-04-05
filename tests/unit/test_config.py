bbcaf4d7d347df72fb194f99cb68115bb5e66252
rsp9u <gctwt852@yahoo.co.jp>
2019-04-06 00:06:33 +0900

    Fix tests for config parser

--- tests/unit/test_config.py
+++ tests/unit/test_config.py
@@ -4,71 +4,22 @@ from side_runner_py import config
 
 
 def test_default_config():
-    os.environ['FOOBAR_CONFIG'] = ""
-    with mock.patch.dict(config.CONFIG_MAP, {'foobar-config': {'value': 'foobar'}}, clear=True):
+    os.environ.pop('SIDE_FOOBAR_CONFIG', None)
+    with mock.patch.dict(config.CONFIG_DEF['items'], {'foobar-config': {'default': 'default-foobar'}}, clear=True):
         config.Config.init()
-        assert config.Config.FOOBAR_CONFIG == "foobar"
+        assert config.Config.FOOBAR_CONFIG == "default-foobar"
 
 
 def test_env_overwrite_default_config():
-    os.environ['FOOBAR_CONFIG'] = "env-foobar"
-    with mock.patch.dict(config.CONFIG_MAP, {'foobar-config': {'value': 'default-foobar'}}, clear=True):
+    os.environ['SIDE_FOOBAR_CONFIG'] = "env-foobar"
+    with mock.patch.dict(config.CONFIG_DEF['items'], {'foobar-config': {'default': 'default-foobar'}}, clear=True):
         config.Config.init()
         assert config.Config.FOOBAR_CONFIG == "env-foobar"
 
 
 def test_arg_overwrite_default_config():
-    os.environ['FOOBAR_CONFIG'] = "env-foobar"
-    with mock.patch.object(config.sys, 'argv', ['prog_name', '--foobar-config=foobar']):
-        with mock.patch.dict(config.CONFIG_MAP, {'foobar-config': {'value': 'default-foobar'}}, clear=True):
+    os.environ['SIDE_FOOBAR_CONFIG'] = "env-foobar"
+    with mock.patch.object(config.sys, 'argv', ['prog_name', '--foobar-config=arg-foobar']):
+        with mock.patch.dict(config.CONFIG_DEF['items'], {'foobar-config': {'default': 'default-foobar'}}, clear=True):
             config.Config.init()
-            assert config.Config.FOOBAR_CONFIG == "foobar"
-
-
-def test_config_type_cast_mixed():
-    config_map = {
-      'env-foobar-config': {'value': 'default-foobar', 'type': int},
-      'env-foobar-config2': {'value': 'default-foobar', 'type': str},
-      'arg-foobar-config': {'value': 'default-foobar', 'type': int}
-    }
-    os.environ['ENV_FOOBAR_CONFIG'] = "1"
-    os.environ['ENV_FOOBAR_CONFIG2'] = "2"
-    with mock.patch.object(config.sys, 'argv', ['prog_name', '--arg-foobar-config=3']):
-        with mock.patch.dict(config.CONFIG_MAP, config_map, clear=True):
-            config.Config.init()
-            assert config.Config.ENV_FOOBAR_CONFIG == 1
-            assert config.Config.ENV_FOOBAR_CONFIG2 == "2"
-            assert config.Config.ARG_FOOBAR_CONFIG == 3
-            assert type(config.Config.ENV_FOOBAR_CONFIG) == int
-            assert type(config.Config.ENV_FOOBAR_CONFIG2) == str
-            assert type(config.Config.ARG_FOOBAR_CONFIG) == int
-
-
-def test_config_type_cast():
-    config_map = {
-      'env-foobar-config': {'value': 'default-foobar', 'type': int},
-      'arg-foobar-config': {'value': 'default-foobar', 'type': int}
-    }
-    os.environ['ENV_FOOBAR_CONFIG'] = "1"
-    with mock.patch.object(config.sys, 'argv', ['prog_name', '--arg-foobar-config=2']):
-        with mock.patch.dict(config.CONFIG_MAP, config_map, clear=True):
-            config.Config.init()
-            assert config.Config.ENV_FOOBAR_CONFIG == 1
-            assert config.Config.ARG_FOOBAR_CONFIG == 2
-            assert type(config.Config.ENV_FOOBAR_CONFIG) == int
-            assert type(config.Config.ARG_FOOBAR_CONFIG) == int
-
-
-def test_config_type_default_cast():
-    config_map = {
-      'env-foobar-config': {'value': 'default-foobar'},
-      'arg-foobar-config': {'value': 'default-foobar'}
-    }
-    os.environ['ENV_FOOBAR_CONFIG'] = "1"
-    with mock.patch.object(config.sys, 'argv', ['prog_name', '--arg-foobar-config=2']):
-        with mock.patch.dict(config.CONFIG_MAP, config_map, clear=True):
-            config.Config.init()
-            assert config.Config.ENV_FOOBAR_CONFIG == "1"
-            assert config.Config.ARG_FOOBAR_CONFIG == "2"
-            assert type(config.Config.ENV_FOOBAR_CONFIG) == str
-            assert type(config.Config.ARG_FOOBAR_CONFIG) == str
+            assert config.Config.FOOBAR_CONFIG == "arg-foobar"

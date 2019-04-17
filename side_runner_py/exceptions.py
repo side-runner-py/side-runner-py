8e7af016901b6a0425aa7d1666a81c6e41280481
rsp9u <gctwt852@yahoo.co.jp>
2019-04-18 00:51:28 +0900

    Add exception classes for assert/verify failure

---
+++ side_runner_py/exceptions.py
@@ -0,0 +1,31 @@
+_msg_fmt = '''Failed to {} (command: {}, target: {})
+Expected
+  {}
+Actual
+  {}'''
+
+
+def _format_msg(exc, kind):
+    return _msg_fmt.format(kind, exc.command_name, exc.target, exc.expected, exc.actual)
+
+
+class AssertionFailure(Exception):
+    def __init__(self, command_name='', target='', expected='', actual=''):
+        self.command_name = command_name
+        self.target = target
+        self.expected = expected
+        self.actual = actual
+
+    def format_msg(self):
+        return _format_msg(self, 'assert')
+
+
+class VerificationFailure(Exception):
+    def __init__(self, command_name='', target='', expected='', actual=''):
+        self.command_name = command_name
+        self.target = target
+        self.expected = expected
+        self.actual = actual
+
+    def format_msg(self):
+        return _format_msg(self, 'verify')

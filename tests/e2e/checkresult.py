77d3a2f38028bf0de681de24eb5e2ab219e022e4
rsp9u <gctwt852@yahoo.co.jp>
2020-06-27 18:29:22 +0900

    Modify e2e result check script to be runnable without emoji

--- tests/e2e/checkresult.py
+++ tests/e2e/checkresult.py
@@ -1,11 +1,16 @@
 import sys
 import json
-from emoji import emojize
 
 
-def print_result(suite_name, test_name, is_failed):
-    result_emoji = ":x:" if is_failed else ":o:"
-    print("{0} {1} : {2}".format(suite_name, test_name, emojize(result_emoji, use_aliases=True)))
+def print_result(suite_name, test_name, is_failed, use_emoji):
+    if use_emoji:
+        from emoji import emojize
+        result_emoji = ":x:" if is_failed else ":o:"
+        result_mark = emojize(result_emoji, use_aliases=True)
+    else:
+        result_mark = "x" if is_failed else "o"
+
+    print("{0} {1} : {2}".format(suite_name, test_name, result_mark))
 
 
 def is_expected(command):
@@ -15,19 +20,34 @@ def is_expected(command):
         return command['is_failed'] is False
 
 
-with open(sys.argv[1]) as f:
-    result = json.load(f)
+def run(result_file, use_emoji):
+    with open(sys.argv[1]) as f:
+        result = json.load(f)
 
-results = []
-for suite in result:
-    suite["name"]
-    for test in suite["tests"]:
-        unexpected = any([not is_expected(c) for c in test["commands"]])
-        print_result(suite["name"], test["name"], unexpected)
-        results.append(unexpected)
+    results = []
+    for suite in result:
+        suite["name"]
+        for test in suite["tests"]:
+            unexpected = any([not is_expected(c) for c in test["commands"]])
+            print_result(suite["name"], test["name"], unexpected, use_emoji)
+            results.append(unexpected)
+
+    if any(results):
+        sys.exit(1)
+    else:
+        sys.exit(0)
 
 
-if any(results):
-    sys.exit(1)
-else:
-    sys.exit(0)
+if __name__ == "__main__":
+    if len(sys.argv) <= 1:
+        print("too few arguments")
+        sys.exit(1)
+
+    result_file = sys.argv[1]
+
+    if len(sys.argv) >= 3 and sys.argv[2] == "--without-emoji":
+        use_emoji = False
+    else:
+        use_emoji = True
+
+    sys.exit(run(result_file, use_emoji))

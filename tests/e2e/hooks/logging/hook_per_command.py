8d88f4ff0dfb39c4c6b813359a7fbe1f99d517bc
rsp9u <gctwt852@yahoo.co.jp>
2020-06-27 18:52:45 +0900

    Add e2e test for hook system

---
+++ tests/e2e/hooks/logging/hook_per_command.py
@@ -0,0 +1,26 @@
+conditions = {
+    'test_project_ids': ['*'],
+    'test_suite_ids': ['*'],
+    'test_ids': ['*'],
+    'test_command_types': ['click'],
+}
+
+
+def _log(pre_post, session_manager, test, test_command, test_command_index):
+    with open("/hook-logs/log.txt", "a") as f:
+        msg_dict = {
+            "session_id": session_manager.driver.session_id,
+            "test_name": test["name"],
+            "command": test_command["command"],
+            "command_index": test_command_index,
+        }
+        print("{}_command_hook: {}".format(pre_post, msg_dict), f)
+        print("{}_command_hook: {}".format(pre_post, msg_dict))
+
+
+def pre_command_run(session_manager, test, test_command, test_command_index):
+    _log("pre", session_manager, test, test_command, test_command_index)
+
+
+def post_command_run(session_manager, test, test_command, test_command_index):
+    _log("post", session_manager, test, test_command, test_command_index)
